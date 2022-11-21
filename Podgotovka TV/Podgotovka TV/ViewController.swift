33daa82dce716275a35fa228c1f7a1c1e6527958
TemaKrisa <btsi1@outlook.com>
2022-11-21 16:26:30 +0300

    comment

---
+++ Podgotovka TV/Podgotovka TV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Podgotovka TV
+//
+//  Created by 35кабинет 6 on 17.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
